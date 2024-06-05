3f4d95d09c09430cb90352bfa24dc6bb05885c50
viethoang261204 <viethoang26122004@gmail.com>
2024-06-05 19:51:38 +0700

    checkout success

--- resources/views/user/checkoutsucces.blade.php
+++ resources/views/user/checkoutsucces.blade.php
@@ -1,20 +1,73 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <title>Bong - Cart</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="/t/img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="/t/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="/t/css/style.css" rel="stylesheet">
 </head>
+
 <body>
+@include('partials.header')
+
+<div class="container-fluid bg-secondary mb-5">
+    <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 300px">
+        <h1 class="font-weight-semi-bold text-uppercase mb-3">Checkout</h1>
+        <div class="d-inline-flex">
+            <p class="m-0"><a href="">Home</a></p>
+            <p class="m-0 px-2">-</p>
+            <p class="m-0">Checkout </p>
+        </div>
+    </div>
+</div>
+
 <form action="cart/checkout" method="post">
     @csrf
     <div id="page-wrap">
         <div class="small-container cart-page">
-            <h1>Checkout Successfully</h1>
-            <a href="/home"><h2 style="text-align: center;">GO BACK</h2></a>
+            <div class="alert alert-success text-center">
+                <h1></i> Checkout Successfully!</h1>
+                <p>Your order has been placed successfully. Thank you for shopping with us!</p>
+                <a href="/home" class="btn btn-primary">Go Back to Home</a>
+            </div>
         </div>
     </div>
+</form>
+@include('partials.footer')
+
+<!-- Back to Top -->
+<a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="/t/lib/easing/easing.min.js"></script>
+<script src="/t/lib/owlcarousel/owl.carousel.min.js"></script>
+
+<!-- Contact Javascript File -->
+<script src="/t/mail/jqBootstrapValidation.min.js"></script>
+<script src="/t/mail/contact.js"></script>
+
+<!-- Template Javascript -->
+<script src="/t/js/main.js"></script>
 </body>
 </html>
+
